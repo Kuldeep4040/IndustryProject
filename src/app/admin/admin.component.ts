c6fce48d1c67de90579128267a49777f6251beb0
Kuldeep4040 <kuldeep.pawar1312@gmail.com>
2018-12-14 12:21:17 -0600

    3rd Commit by Kuldeep pawar

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,7 +3,20 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms';  
 
 import {MatDialog, MatDialogActions, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+const COLORS: string[] = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
 
 export interface DialogData {
@@ -37,7 +50,7 @@ constructor(public dialog: MatDialog, private fb: FormBuilder) {
 
 openDialog(): void {
   const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-    width: '250px',
+    width: '400px',
     data: {name: this.name, animal: this.animal}
   });
 
@@ -45,21 +58,63 @@ openDialog(): void {
     console.log('The dialog was closed');
     this.animal = result;
   });
+  
 }
 
 }
+
 @Component({
-  selector: 'inward-modal',
-  templateUrl: './inward-modal.html',
+  selector: 'admin.component.inward-modal',
+  templateUrl: './admin.component.inward-modal.html',
 })
 export class DialogOverviewExampleDialog {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
+  dataSource: MatTableDataSource<UserData>;
 
-  constructor(
-    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  onNoClick(): void {
-    this.dialogRef.close();
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+    
+
 }
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
+
+  
+}
+
+  // onNoClick(): void {
+  //   this.dialogRef.close();
+  // }
