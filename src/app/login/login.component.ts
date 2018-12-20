6de87fb7e74fe715e2b5f3b033f7303e30a5ecd4
Kuldeep4040 <kuldeep.pawar1312@gmail.com>
2018-12-20 17:31:46 -0600

    5th commit by kuldeep pawar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms';  
+
 
 
 @Component({
@@ -10,25 +11,68 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent  {
-  
-  constructor(private router : Router) {
-  }
+  loginForm: FormGroup;  
+  username:string='';
+  password:string='';  
 
-  username : string
-  password : string
-
-  login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["/admin"]);
+  constructor(private router : Router, private fb: FormBuilder) {
+    this.loginForm = fb.group({  
+      'username' : [null, Validators.required],  
+      'password' : [null, Validators.required],  
      
-    }
-    else if(this.username == 'kd' && this.password == 'kd'){
-      this.router.navigate(["/dashboard"]);
-    }
-    else {
       
-      alert("Invalid credentials");
-     
-    }
+    });  
+  
   }
+
+  // username : string
+  // password : string
+
+  // login() : void {
+  //   if(this.username == 'admin' && this.password == 'admin'){
+  //    this.router.navigate(["/admin"]);
+     
+  //   }
+  //   else if(this.username == 'kd' && this.password == 'kd'){
+  //     this.router.navigate(["/dashboard"]);
+  //   }
+  //   else {
+      
+  //     alert("Invalid credentials");
+     
+  //   }
+  // }
+
+  onFormSubmit(form:NgForm)  
+{  
+  console.log(this.loginForm.value.password);
+  if(this.username == 'admin' && this.password == 'admin'){
+     this.router.navigate(["/admin"]);
+       
+       }
+       else if(this.username == 'kd' && this.password == 'kd'){
+         this.router.navigate(["/dashboard"]);
+       }
+       else {
+        
+       alert("Invalid credentials");
+       
+       }
+  // localStorage.setItem('email', form.value.username);
+  // localStorage.setItem('pass', form.value.password);
+  // var uname=localStorage.getItem('email');
+  // var pass=localStorage.getItem('pass');
+  // if(uname == 'admin' && pass == 'admin'){
+  //    this.router.navigate(["/admin"]);
+       
+  //     }
+  //      else if(uname == 'kd' && pass == 'kd'){
+  //       this.router.navigate(["/dashboard"]);
+  //     }
+  //     else {
+        
+  //     alert("Invalid credentials");
+       
+  //     }
+}
 }
